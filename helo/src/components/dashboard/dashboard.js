4ee0f6e3fdf2b2f35694170396c69a49b1b099e1
unknown <jdavid.stevens@gmail.com>
2018-10-03 15:09:24 -0600

    Dashboard_complete

--- helo/src/components/dashboard/dashboard.js
+++ helo/src/components/dashboard/dashboard.js
@@ -6,7 +6,7 @@ import { updateUserData } from '../../ducks/reducer';
 import Home from '../../assets/home.png';
 import Search from '../../assets/search.png';
 import './dashboard.css';
-import Recommend from './recommend/recommend';
+// import Recommend from './recommend/recommend';
 
 
 class Dashboard extends Component {
@@ -14,10 +14,15 @@ class Dashboard extends Component {
     super()
 
     this.state = {
-      user: {}
+      robots:[],
+      user: {},
+      attribute: ''
     }
 
     this.logout = this.logout.bind(this);
+    this.handleAttribute = this.handleAttribute.bind(this);
+    this.handleAddFriend = this.handleAddFriend.bind(this);
+    
   }
 
   componentDidMount() {
@@ -32,10 +37,23 @@ class Dashboard extends Component {
       })
 
     axios.get('/api/user/self').then(response => {
-      console.log("self:", response.data[0]);
+      // console.log("self:", response.data[0]);
       this.setState({ user: response.data[0] })
     })
+
+    axios.get('/api/user/list').then(response => {
+      this.setState({ robots: response.data })
+  })
   }
+
+  handleAttribute(event) {
+    this.setState({ attribute: event.target.value })
+}
+
+  handleAddFriend(id) {
+    axios.post(`/api/friend/add`, { id })
+        .then(results => { this.setState({ robots: results.data }) })
+}
 
   logout() {
     axios.post('/api/auth/logout').then(res => {
@@ -45,6 +63,29 @@ class Dashboard extends Component {
 
 
   render() {
+
+    console.log("attribute", this.state.attribute)
+        let roboFilter = this.state.robots.filter(element=>{
+            return element[this.state.attribute]===this.state.user[this.state.attribute]
+        }
+        ).map((element, index) => {
+            
+            return (
+                
+                    <div className="recommended-friends-info" key={index}>
+                        <img src={element.picture} alt='' className="profile-img" />
+                        <div className="recommended-friend-name-box">
+                            <h5 className="recommended-first-name" >{element.first_name}</h5>
+                            <h5 className="recommended-last-name" >{element.last_name}</h5>
+                        </div>
+                        <div className="add-friend-box">
+                            <button className="add-friend" onClick={() => this.handleAddFriend(element.id)}>Add Friend</button>
+                        </div>
+                        </div>
+            )
+        })
+
+        console.log("robofilter",roboFilter)
 
     return (
       <div className="dashboard-page">
@@ -75,12 +116,35 @@ class Dashboard extends Component {
             </div>
             <div className='welcome'><p>Welcome to Helo! Find recommended friends based on your similarities, and even search for them by name. The more you update your profile, the better recommendations we can make!</p></div>
           </div>
-          <div>
-            <Recommend />
-          </div>
 
+          <div className="recommended-friends-box">
+          <div className="recommended-friends-heading">
+          <h2 className="recommended-friends-title">Recommended Friends</h2>
+          <div className="sort-container">
+                        <h5 className="sorted-by-title">Sorted by</h5>
+                        <select onChange={this.handleAttribute} className="recommended-friend-sorter">
+                            <option>--Select--</option>
+                            <option value="first_name">First Name</option>
+                            <option value="last_name">Last Name</option>
+                            <option value="gender">Gender</option>
+                            <option value="hair">Hair Color</option>
+                            <option value="eye">Eye Color</option>
+                            <option value="hobby">Hobby</option>
+                            <option value="bday">Birthday Day</option>
+                            <option value="bmonth">Birthday Month</option>
+                            <option value="byear">Birthday Year</option>
+                        </select>
+                    </div>
+          
+          </div>
+          <div className="recommended-friends">
+            {(roboFilter.length >= 1)
+              ?(roboFilter):(<p className="no-recommendations">No Recommendations</p>)}
+          </div>
+          </div>
         </div>
       </div>
+      
     )
   }
 }
--- helo/src/components/dashboard/recommend/recommend.js
+++ helo/src/components/dashboard/recommend/recommend.js
@@ -7,6 +7,7 @@ export default class Recommend extends Component {
 
         this.state = {
             robots: [],
+            self: {},
             attribute: ''
         }
 
@@ -19,12 +20,16 @@ export default class Recommend extends Component {
         axios.get('/api/user/list').then(response => {
             this.setState({ robots: response.data })
         })
+
+        axios.get('/api/user/self').then(response => {
+            this.setState({ self: response.data[0] })
+        })
     }
 
     handleAttribute(event) {
         this.setState({ attribute: event.target.value })
-      }
-      
+    }
+
     handleAddFriend(id) {
         axios.post(`/api/friend/add`, { id })
             .then(results => { this.setState({ robots: results.data }) })
@@ -33,44 +38,77 @@ export default class Recommend extends Component {
 
     render() {
 
-        console.log("recommend test:",this.state.robots)
-
-        let filteredRobos = this.state.robots.map((element, index) => {
+        console.log("attribute", this.state.attribute)
+        let roboFilter = this.state.robots.filter(element=>{
+            return element[this.state.attribute]===this.state.self[this.state.attribute]
+        }
+        ).map((element, index) => {
+            if(element.length===0){
+                return <h1>No Recommendations</h1>
+            }else{
             return (
                 <div className="recommended-friends" key={index}>
-                <div className="recommended-friends-info" >
-                    <img src={element.picture} alt='' className="profile-img" />
-                    <div className="recommended-friend-name-box">
-                        <h5 className="recommended-first-name" >{element.first_name}</h5>
-                        <h5 className="recommended-last-name" >{element.last_name}</h5>
-                    </div>
-                    <div className="add-friend-box">
-                        <button className="add-friend" onClick={() => this.handleAddFriend(element.id)}>Add Friend</button>
+                    <div className="recommended-friends-info" >
+                        <img src={element.picture} alt='' className="profile-img" />
+                        <div className="recommended-friend-name-box">
+                            <h5 className="recommended-first-name" >{element.first_name}</h5>
+                            <h5 className="recommended-last-name" >{element.last_name}</h5>
+                        </div>
+                        <div className="add-friend-box">
+                            <button className="add-friend" onClick={() => this.handleAddFriend(element.id)}>Add Friend</button>
+                        </div>
                     </div>
                 </div>
-                </div>
-            )
+            )}
         })
+
+        let roboLastName = this.state.robots.filter(element=>{
+            return this.state.attribute==="Last Name" && element.last_name===this.state.self.last_name
+        }
+        ).map((element, index) => {
+            console.log("if test",element)
+            if(roboLastName.length===0 ){
+                
+                return <h1>No Recommendations</h1>
+            }else{
+            return (
+                <div className="recommended-friends" key={index}>
+                    <div className="recommended-friends-info" >
+                        <img src={element.picture} alt='' className="profile-img" />
+                        <div className="recommended-friend-name-box">
+                            <h5 className="recommended-first-name" >{element.first_name}</h5>
+                            <h5 className="recommended-last-name" >{element.last_name}</h5>
+                        </div>
+                        <div className="add-friend-box">
+                            <button className="add-friend" onClick={() => this.handleAddFriend(element.id)}>Add Friend</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        })
+
         return (
             <div className="recommended-friends-box">
-            <div className="recommended-friends-heading">
-              <h2 className="recommended-friends-title">Recommended Friends</h2>
-              <div className="sort-container">
-                <h5 className="sorted-by-title">Sorted by</h5>
-                <select onChange={this.handleAttribute} className="recommended-friend-sorter">
-                  <option>First Name</option>
-                  <option>Last Name</option>
-                  <option>Gender</option>
-                  <option>Hair Color</option>
-                  <option>Eye Color</option>
-                  <option>Hobby</option>
-                  <option>Birthday Day</option>
-                  <option>Birthday Month</option>
-                  <option>Birthday Year</option>
-                </select>
-              </div>
-            </div>
-            <div>{filteredRobos}</div>
+                <div className="recommended-friends-heading">
+                    <h2 className="recommended-friends-title">Recommended Friends</h2>
+                    <div className="sort-container">
+                        <h5 className="sorted-by-title">Sorted by</h5>
+                        <select onChange={this.handleAttribute} className="recommended-friend-sorter">
+                            <option value="first_name">First Name</option>
+                            <option value="last_name">Last Name</option>
+                            <option value="gender">Gender</option>
+                            <option value="hair">Hair Color</option>
+                            <option value="eye">Eye Color</option>
+                            <option value="hobby">Hobby</option>
+                            <option value="bday">Birthday Day</option>
+                            <option value="bmonth">Birthday Month</option>
+                            <option value="byear">Birthday Year</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    {roboFilter} 
+                </div>
             </div>
         )
     }
